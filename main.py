b523a58f491716feb81dd624b6eb2c6ee50ae8ba
Messbah.A <Messbah.A@Gmail.com>
2025-09-20 19:23:43 +0330

    debugged

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-
 from data_access.database_manager import create_table_user
 create_table_user()
-
-# TODO making sure database path is always found! ;-(((
-# TODO absolute path v relative path    rwc?
-# import sys
-# sys.path.append("C:\\Users\farzi\Downloads\User_Management_App")
 
 from view import user_view
 
